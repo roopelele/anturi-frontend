551fe60c74d005adefd980ea58378ef340b4c15a
vaino <vaino.granat1@gmail.com>
2021-01-10 02:41:33 +0200

    Added Chart component..

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>Roopen asunnon lämpötilat</h1>
-    </div>
+    <>
+      <div>
+        <h1>Roopen asunnon lämpötilat</h1>
+      </div>
+      <div>
+        <p>Kys</p>
+      </div>
+      {<Chart/>}
+    </>
   )
 }
+
+const Chart = () => {
+  return (
+    <>
+      <h2>Here is your chart asshole</h2>
+    </>
+  )
+}
+
 
 export default App
